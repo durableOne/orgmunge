98ded52dbdda68df8690dd1f98099ab48b1d1143
Joseph Riad <joseph.samy.albert@gmail.com>
2023-09-25 23:11:29 -0500

    Renamed test file with the new method name

---
+++ tests/test_get_all_headings.py
@@ -0,0 +1,18 @@
+from orgmunge import Org
+import re
+
+def test_get_all_headings():
+    test_file = '''* Node 1
+** Subnode 1
+** Subnode 2
+* Node 2
+** Subnode 1
+* Node 3
+** Subnode 1
+*** Subnode 1
+*** Subnode 2
+    '''
+    node_titles = re.sub(r'\*+\s+', '', test_file.strip()).split('\n')
+
+    parsed = Org(test_file, from_file=False)
+    assert [n.title for n in parsed.get_all_headings()] == node_titles
