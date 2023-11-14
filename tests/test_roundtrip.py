433f7902424a2816557c67dc623313ddae79282f
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-11-14 19:13:59 +0000

    tests : failing test - non-standard character not parsed.

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -7,22 +7,31 @@ from orgmunge import Org
 import pytest
 
 
-EXAMPLE_1 = """\
+EXAMPLES = (
+    """\
 * Calculation TODO
 ** Input
 3+4+5+6
 ** Evaluation
-"""
+""",
+    """\
+* Parse weird characters
+
+There is a weird character between these two   words.
+""",
+)
 
 
-@pytest.mark.parametrize("text", (EXAMPLE_1,))
+@pytest.mark.parametrize("text", EXAMPLES)
 def test_roundtrip(text):
-    fake_todos = {'todo_states': {'fake_todo': 'TDO'},
-                  'done_states': {'fake_done': 'DNE'},}
-    parsed = Org(EXAMPLE_1, from_file=False, todos=fake_todos)
+    fake_todos = {
+        "todo_states": {"fake_todo": "TDO"},
+        "done_states": {"fake_done": "DNE"},
+    }
+    parsed = Org(text, from_file=False, todos=fake_todos)
 
     # Doesn't produce errors when roundtripped
     Org(str(parsed), from_file=False, todos=fake_todos)
 
     # Produces identical output roundtripped
-    assert str(parsed) == EXAMPLE_1
+    assert str(parsed) == text
