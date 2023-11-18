7c17dc8532857c648542c37d645d7e52efe0c350
Colm O'Connor <colm.oconnor.github@gmail.com>
2023-11-18 12:06:05 +0000

    tests : failing test - non-standard character not parsed.

--- tests/test_roundtrip.py
+++ tests/test_roundtrip.py
@@ -9,7 +9,7 @@ import pytest
 
 EXAMPLES = (
     """\
-* Calculation TODO
+* TODO Calculation
 ** Input
 3+4+5+6
 ** Evaluation
@@ -21,22 +21,26 @@ There is a weird {} character {} between 2 of these words.
 """.format(chr(160), chr(8239), chr(160)),
 """\
 * A normal title
-* TODO A title with the word TODO in the title which triggers a syntax error
+* TODO A title with the -- word TODO in the title which triggers a syntax error
 * TODO Another normal title.
 """,
+"""\
+* :atitlewithjustatag:
+"""
 )
 
 
 @pytest.mark.parametrize("text", EXAMPLES)
 def test_roundtrip(text):
-    fake_todos = {
-        "todo_states": {"fake_todo": "TDO"},
-        "done_states": {"fake_done": "DNE"},
+    todo_and_done_states = {
+        "todo_states": {"todo": "TODO"},
+        "done_states": {"done": "DONE"},
     }
-    parsed = Org(text, from_file=False, todos=fake_todos)
+    # Shouldn't produce errors when parsed
+    parsed = Org(text, from_file=False, todos=todo_and_done_states)
 
-    # Doesn't produce errors when roundtripped
-    Org(str(parsed), from_file=False, todos=fake_todos)
+    # Shouldn't produce errors when roundtripped
+    Org(str(parsed), from_file=False, todos=todo_and_done_states)
 
-    # Produces identical output roundtripped
+    # Should produce identical output roundtripped
     assert str(parsed) == text
